f814d4ae4d8bcafa4fb09efb9679c7c8de1f4ac8
saralimbu2017 <iamsebi7@gmail.com>
2022-03-02 22:11:26 +1100

    Deleted Ruby folder

--- ruby/README.md
+++
@@ -1,5 +0,0 @@
-## Running tests
-```
-bundle install
-bundle exec rspec tests/
-```
--- ruby/tasks/2-even.rb
+++
@@ -1,9 +0,0 @@
-#! rspec ./../tests/2-even.rb
-
-#
- # Returns true if the input is even
- # @param {Number} integer The integer to check.
- # @returns {Boolean} Whether the input is even.
-#
-def is_even_number(integer)
-end
