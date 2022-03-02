51d25a34ee52608e4b8c9000fde83b81522f8420
saralimbu2017 <iamsebi7@gmail.com>
2022-03-02 22:34:27 +1100

    Solution task 4-longest.js

--- js/tasks/4-longest.js
+++ js/tasks/4-longest.js
@@ -6,5 +6,17 @@
  * @returns {String} The longest string from within the input array.
  */
 export default function longestString(strings) {
-    
+    //declare a variable as longestString
+    var longestString = "";
+    //loop through the array of strings
+     
+    strings.forEach( currentElement => {
+      //check if data type of each element  is string
+      // compare its length with longestString length
+      //if it's length is greater longest string is current element.
+      if(typeof(currentElement) === 'string' && currentElement.length > longestString.length) {
+        longestString = currentElement;
+      }
+    })
+   return longestString;
 }
