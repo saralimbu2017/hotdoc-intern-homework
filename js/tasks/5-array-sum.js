154b0221362c1cd3e50b883242baf1767ec27681
saralimbu2017 <iamsebi7@gmail.com>
2022-03-02 22:49:50 +1100

    Solution for task5-array-sum

--- js/tasks/5-array-sum.js
+++ js/tasks/5-array-sum.js
@@ -6,5 +6,18 @@
  * @returns {Number} The sum of all integers contained in the input, at any level.
  */
 export default function arraySum(startOfTree) {
-
+  //declare a variable sum
+  var sum = 0;
+  startOfTree.forEach( element => {
+    //check if current element is number
+    //add it to sum
+    if(typeof(element) === 'number') {
+      sum += element;
+    } else if (Array.isArray(element)) {
+      //if current element is array
+      //call function recursively
+      sum += arraySum(element);
+    }
+  });
+  return sum;
 }
